0d0d94f8ef24354e0ccaf948a14d45d9c967ae9b
Jeniffervp <737@holbertonschool.com>
2020-05-13 12:22:34 -0500

    [Add] more coins and score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,28 @@
 public class PlayerController : MonoBehaviour
 {
     public float speed = 500f;
-
     public Rigidbody rigb;
-
+    private int score = 0;
     // Update is called once per frame
     public void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
             rigb.AddForce(0, 0, speed * Time.deltaTime);
-        if(Input.GetKey(KeyCode.RightArrow))
-            rigb.AddForce(speed* Time.deltaTime, 0, 0);
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
+            rigb.AddForce(speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
             rigb.AddForce((-1 * speed) * Time.deltaTime, 0, 0);
-        if(Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
             rigb.AddForce(0, 0, (-1 * speed) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score += 1;
+            Debug.Log("Score: " + score);
+            Destroy (other.gameObject);
+        }
     }
 }
